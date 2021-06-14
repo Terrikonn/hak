70befb17c5dd1ea12373bba1cd0710e60b03f8e8
remimimimi <valent.xarin@gmail.com>
2021-06-14 13:39:56 +0300

    Change alignment to 6

--- klog/src/lib.rs
+++ klog/src/lib.rs
@@ -115,7 +115,7 @@ impl Log for KernelLogger {
             return;
         }
         let level_string_color = match record.level() {
-            Level::Emerg=> DynColors::Ansi(AnsiColors::Magenta),
+            Level::Emerg => DynColors::Ansi(AnsiColors::Magenta),
             Level::Alert => DynColors::Ansi(AnsiColors::Yellow),
             Level::Crit => DynColors::Ansi(AnsiColors::Red),
             Level::Error => DynColors::Ansi(AnsiColors::BrightRed),
@@ -130,7 +130,7 @@ impl Log for KernelLogger {
             record.module_path().unwrap_or_default()
         };
         println!(
-            "{:>9} [{}] {}",
+            "{:>6} [{}] {}",
             record.level().color(level_string_color),
             target,
             record.args()
